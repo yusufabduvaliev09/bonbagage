2bf6b00fb4bd6cf0320b064cf01da4d070d2ce3b
Matvey0808 <avto.mat.2000g@gmail.com>
2026-05-07 14:16:09 +0700

    fix: delete Row from dialog and variable transfer

--- lib/widget/dialog_editJourney_widget.dart
+++ lib/widget/dialog_editJourney_widget.dart
@@ -12,26 +12,29 @@ void showEditJourneyDialog(BuildContext context) {
 class DialogEditjourneyWidget extends StatelessWidget {
   const DialogEditjourneyWidget({super.key});
 
+  static const BorderRadius border = BorderRadius.all(Radius.circular(12));
+  static const BorderSide borderSide = BorderSide(
+    width: 3,
+    color: Colors.black26,
+  );
+
+  static final elevatedButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: Colors.black12,
+    shape: RoundedRectangleBorder(borderRadius: border),
+  );
+
+  static final focusedBorderTextField = OutlineInputBorder(
+    borderRadius: border,
+    borderSide: borderSide,
+  );
+
+  static final enableBorderTextField = OutlineInputBorder(
+    borderRadius: border,
+    borderSide: borderSide,
+  );
+
   @override
   Widget build(BuildContext context) {
-    final BorderRadius border = BorderRadius.all(Radius.circular(12));
-    final BorderSide borderSide = BorderSide(width: 3, color: Colors.black26);
-
-    final elevatedButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: Colors.black12,
-      shape: RoundedRectangleBorder(borderRadius: border),
-    );
-
-    final focusedBorderTextField = OutlineInputBorder(
-      borderRadius: border,
-      borderSide: borderSide,
-    );
-
-    final enableBorderTextField = OutlineInputBorder(
-      borderRadius: border,
-      borderSide: borderSide,
-    );
-
     return AlertDialog(
       content: TextField(
         cursorColor: Colors.black26,
@@ -41,26 +44,21 @@ class DialogEditjourneyWidget extends StatelessWidget {
         ),
       ),
       actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton(
-              style: elevatedButtonStyle,
-              onPressed: () => Navigator.pop(context),
-              child: Text(
-                "Отмена",
-                style: TextStyle(fontSize: 14, color: Colors.black54),
-              ),
-            ),
-            ElevatedButton(
-              style: elevatedButtonStyle,
-              onPressed: () => Navigator.pop(context),
-              child: Text(
-                "Добавить",
-                style: TextStyle(fontSize: 14, color: Colors.black54),
-              ),
-            ),
-          ],
+        ElevatedButton(
+          style: elevatedButtonStyle,
+          onPressed: () => Navigator.pop(context),
+          child: Text(
+            "Отмена",
+            style: TextStyle(fontSize: 14, color: Colors.black54),
+          ),
+        ),
+        ElevatedButton(
+          style: elevatedButtonStyle,
+          onPressed: () => Navigator.pop(context),
+          child: Text(
+            "Добавить",
+            style: TextStyle(fontSize: 14, color: Colors.black54),
+          ),
         ),
       ],
     );
